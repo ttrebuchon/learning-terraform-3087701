fc68adf808cdbce005a832d65f4901a3c0edf95b
ttrebuchon <ttrebuchon@live.com>
2024-02-28 11:51:19 -0500

    Create security group. Parameterize LinkedIn tag

--- main.tf
+++ main.tf
@@ -14,11 +14,58 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
-    Name = "HelloWorld"
+    Name = var.linkedin_learning_tag
   }
+}
+
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description =  "Allow http/https in and all outbound"
+
+  vpc_id  = data.aws_vpc.default.id
+
+  tags = {
+    Name = var.linkedin_learning_tag
+  }
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "linkedin_learning_tag"  {
+    description = "Label for 'LinkedIn Learning Terraform' resources"
+    default     = "LinkedIn Learning Terraform"
+}
